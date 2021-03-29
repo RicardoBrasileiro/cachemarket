7279b452055bc71012a1a83b9a61513b0ec3b28c
Jose Ricardo <jricardobgoncalves@gmail.com>
2021-03-29 11:52:06 -0300

    Correção de erros de lógica

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,11 +6,13 @@ module.exports = {
 
     async index(req, res) {
 
-        const response = await query(
+        await query(
             'SELECT * FROM products ORDER BY p_name ASC'
-        );
-
-        res.status(200).send({ products: response.rows });
+        ).then(async (response) => {
+            return res.status(200).send({ message: "Produtos retornados!", products: response.rows });
+        }, (error) => {
+            return res.status(400).send({ message: "Ocorreu um erro!", products: null });
+        });
     },
 
     async show(req, res) {
@@ -21,27 +23,28 @@ module.exports = {
             if (reply != null) {
 
                 const value = JSON.parse(reply.toString());
-                return res.status(200).send({ product: value });
+                return res.status(200).send({ message: "Produto encontrado!", product: value });
 
             } else {
 
-                const response = await query(
+                await query(
                     'SELECT * FROM products WHERE code = $1',
                     [id]
-                );
+                ).then(async (response) => {
 
-                if (response.rows[0]) {
+                    if (response.rows[0]) {
 
-                    const value = response.rows[0];
-                    client.setex(id, (60 * 60), JSON.stringify(value));
+                        const value = response.rows[0];
+                        client.setex(id, (60 * 60), JSON.stringify(value));
 
-                    return res.status(200).send({ product: value });
+                        return res.status(200).send({ message: "Produto encontrado!", product: value });
 
-                } else {
+                    } else {
 
-                    return res.status(400).send({ product: null });
+                        return res.status(404).send({ message: "Produto não encontrado!", product: null });
 
-                }
+                    }
+                });
 
             }
         })
@@ -54,46 +57,53 @@ module.exports = {
         await query(
             'INSERT INTO products (code, p_name, quantity, price) VALUES ($1, $2, $3, $4)',
             [code, p_name, quantity, price]
-        );
-
-        res.status(201).send({
-            message: "Produto adicionado!",
-            body: {
-                product: { code, p_name, quantity, price }
-            },
+        ).then(async () => {
+            return res.status(201).send({ message: "Produto criado!", product: { code, p_name, quantity, price } });
+        }, (error) => {
+            return res.status(400).send({ message: "Produto já existe!", product: null });
         });
     },
 
     async update(req, res) {
 
         const { id } = req.params;
-        const { code, p_name, quantity, price } = req.body;
+        const { p_name, quantity, price } = req.body;
 
-        const { rows } = await query(
-            'UPDATE products SET code = $1, p_name = $2, quantity = $3, price = $4 WHERE code = $5',
-            [code, p_name, quantity, price, id]
-        );
+        await query(
+            'UPDATE products SET p_name = $1, quantity = $2, price = $3 WHERE code = $4',
+            [p_name, quantity, price, id]
+        ).then(async (response) => {
 
-        res.status(200).send({ message: "Produto atualizado!", product: rows });
+            client.get(id, async (err, reply) => {
+                if (reply != null) {
+                    client.del(id);
+                }
+            });
+
+            if (response.rowCount) return res.status(200).send({ message: "Produto atualizado!", product: { code: id, p_name, quantity, price } });
+
+            return res.status(404).send({ message: "Produto não encontrado!", product: null });
+        });
     },
 
     async destroy(req, res) {
 
         const { id } = req.params;
 
-        client.get(id, async (err, reply) => {
-            if (reply != null) {
-
-                client.del(id);
-
-            }
-        });
-
         await query(
             'DELETE FROM products WHERE code = $1',
             [id]
-        );
+        ).then(async (response) => {
 
-        return res.status(200).send({ message: "Produto excluido!" });
+            client.get(id, async (err, reply) => {
+                if (reply != null) {
+                    client.del(id);
+                }
+            });
+
+            if (response.rowCount) return res.status(200).send({ message: "Produto excluído!", product: null });
+
+            return res.status(404).send({ message: "Produto não encontrado!", product: null });
+        });
     },
 };
